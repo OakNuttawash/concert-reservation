394d1684fd9037d5d68fc38e329dc9c2a0bed7cb
OakNuttawash <oak.mirthful@gmail.com>
2025-05-20 22:35:39 +0700

    feat: setup generate type from backend to frontend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,6 +17,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
 
   SwaggerModule.setup('swagger', app, document);
 
