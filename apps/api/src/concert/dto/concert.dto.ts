0403e2a46bd12f832852c8739e9beee042af32db
OakNuttawash <oak.mirthful@gmail.com>
2025-05-20 21:23:43 +0700

    feat: setup validator

--- apps/api/src/concert/dto/concert.dto.ts
+++ apps/api/src/concert/dto/concert.dto.ts
@@ -1,8 +1,23 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
 import { ReservationStatus } from '../entities/reservation.entity';
 
 export class CreateConcertDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
+
+  @IsNumber()
+  @Min(10, { message: 'Total seat must be at least 10 seat' })
   totalSeat: number;
+
+  @IsString()
+  @MinLength(10, { message: 'Description must be at least 10 characters long' })
   description: string;
 }
 
